3460d9bc0e93d9156dff8d6d2ac7aaf8fecca48c
seksit39 <seksit39@outlook.com>
2017-03-06 20:49:04 +0700

    Update test-pwm.ino

--- test-pwm/test-pwm.ino
+++ test-pwm/test-pwm.ino
@@ -9,5 +9,6 @@ void loop() {
   for(int i=0 ; i<8 ; i++){
     int pwmOut = map(sine[i],0,4095,0,255); // Mapping from 0-4095 to 0-255
     analogWrite(9,pwmOut); 
+    delayMicroseconds(400);                 // Delay for change frequncy from 840 Hz to 200 Hz
   }
 }
